fdc02bd5df976ce5f30446ad3e28c27162f5efb6
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-07-13 15:39:08 +0300

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
+  const name = 0;
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">S
+        <a href="https://vite.dev" target="_blank">
+          S
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -17,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1 + name)}>
           count is {count}
         </button>
         <p>
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
